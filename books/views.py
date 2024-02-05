d4baf62e8c8379c10f20f26a42b901e7246a86cc
Fari <Farzad.mahmoudiyekta@gmail.com>
2024-02-05 22:10:55 +0330

    bookdetail was added

--- books/views.py
+++ books/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . import models
 # Create your views here.
 
 
-class BookList(ListView):
+class BookListView(ListView):
     model = models.Book
     template_name = "books/book_list.html"
+
+
+class BookDetailView(DetailView):
+    model = models.Book
+    template_name = "books/book_detail.html"
