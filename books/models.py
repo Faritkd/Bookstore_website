b3473104123ffcc75f37736569d19e9d54d360e8
Farzad <Farzad.mahmoudiyekta@gmail.com>
2024-02-22 21:35:44 +0330

    Review model was added

--- books/models.py
+++ books/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.text import slugify
 # Create your models here.
@@ -16,3 +17,13 @@ class Book(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         return super().save()
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    review = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.review
