92a1b1e87fe3fedee0d4e5f3e64de2eca1db94c6
Farzad <Farzad.mahmoudiyekta@gmail.com>
2024-02-25 11:00:07 +0330

    Search bar was added

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,5 @@ from books import views
 urlpatterns = [
     path('', views.BookListView.as_view(), name="book_list"),
     path('<slug:slug>/', views.BookDetailView.as_view(), name="book_detail"),
+    path('search_results', views.SearchResultsListView.as_view(), name="search_results")
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 from . import models
+from .models import Book
+
+
 # Create your views here.
 
 
@@ -15,3 +19,13 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = "books/book_detail.html"
     login_url = "account_login"
     permission_required = "books.special_status"
+
+
+class SearchResultsListView(ListView):
+    model = models.Book
+    template_name = "books/search_results.html"
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Book.objects.filter(Q(title__icontains=query) | Q(author__icontains=query))
+
